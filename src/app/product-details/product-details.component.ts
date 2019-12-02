1ccbf90d4207103b1e6a9df321a7e52dc2673005
Alvaroggbm <alvaro.garcia@wolterskluwer.com>
2019-12-02 15:23:33 +0100

    2 files modified, 1 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -10,13 +10,16 @@ import { CartService } from '../cart.service';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
+  product;
   constructor( private route: ActivatedRoute,
     private cartService: CartService
 
   ) { }
 
   ngOnInit() {
+     this.route.paramMap.subscribe(params => {
+    this.product = products[+params.get('productId')];
+  });
   }
 
    addToCart(product) {
